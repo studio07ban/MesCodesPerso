7f6a7e42d094c986ce7409b9f2f4e968bbfd88d0
Your namepierreBeauchesne <pierre.beauchesne@gmail.fr>
2025-10-15 20:10:32 +0200

    MesCodesPerso

---
+++ ExoFonctionScore.js
@@ -0,0 +1,15 @@
+//Créer une fonction score qui prend en paramètre un nombre et affiche “C’est gagné” si le nombre est supérieur ou égal à 80, 
+// “Presque !” si le score est compris entre 50 et 80 non inclus 
+// et “Retente ta chance !” si le score est strictement inférieur à 50
+
+function score (79) {
+    if (score >= 80) {
+       console.log("C'est gagné")
+    } else if (score = [50-80]) {
+        console.log("Presque")
+    } else (score >= 50) {
+        console.log("Retente ta chance !")
+    }
+return score;
+
+}
